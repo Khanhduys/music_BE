f7f38e796ce3ce0517f06993b0c852a854f64fb7
khanh <khanhduys923@example.com>
2022-08-08 10:09:51 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,11 @@ import db from '../models/index'
 import userService from "../services/userSevices"
 
 let allUser=async(req,res)=>{
-    let data
-      try{
-         data=await db.users.findAll()
-        console.log(data)
-      } catch(e){
-        console.log(e)
-      }
-    
-      return  res.render({data:JSON.stringify(data)}) // ddua data ra view
+   
+  let message=  await userService.getAllUser()
+  console.log(message)
+   return res.send(message)
+      
     }
 let addUser=async(req,res)=>{
  
