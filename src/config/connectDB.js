db99699e6fe07e76dbe819d4c15136220f8c1168
khanh <khanhduys923@example.com>
2022-08-08 09:54:15 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
-
+require ("dotenv").config()
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('music', 'root', null, {
-  host: 'localhost', // co the la localhost hoawc dia chi server cua ta
+const sequelize = new Sequelize(process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD, {
+  host:process.env.DB_HOST, // co the la localhost hoawc dia chi server cua ta
   dialect: "mysql",
   logging:false // khoong cho phep in ra cau lenh select
 });
